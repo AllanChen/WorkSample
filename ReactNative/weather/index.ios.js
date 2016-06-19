b28403ff15b8cac52fb1e8ea329598da98705cb8
allanchan <allanchanni@gmail.com>
2016-06-19 18:16:34 +0800

    work

--- ReactNative/weather/index.ios.js
+++ ReactNative/weather/index.ios.js
@@ -9,45 +9,84 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TouchableHighlight,
+  navBar,
   View
 } from 'react-native';
 
-class weather extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+// var GiftedListView = require('react-native-gifted-listview');
+var weather = require('./tableview.ios.js')
+// class nam extends Component {
+//    _onFetch(page = 1, callback, options) {
+//     setTimeout(() => {
+//       var rows = ['row '+((page - 1) * 3 + 1), 'row '+((page - 1) * 3 + 2), 'row '+((page - 1) * 3 + 3)];
+//       if (page === 3) {
+//         callback(rows, {
+//           allLoaded: true, // the end of the list is reached
+//         });        
+//       } else {
+//         callback(rows);
+//       }
+//     }, 1000); // simulating network fetching
+//   }
+//   _onPress(rowData) {
+//     console.log(rowData+' pressed');
+//   }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+//   /**
+//    * Render a row
+//    * @param {object} rowData Row data
+//    */
+//   _renderRowView(rowData) {
+//     return (
+//       <TouchableHighlight
+//         style={styles.row}
+//         underlayColor='#c8c7cc'
+//         onPress={() => this._onPress(rowData)}
+//       >  
+//         <Text>{rowData}</Text>
+//       </TouchableHighlight>
+//     );
+//   }
+  
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.navBar} />
+//         <GiftedListView
+//           rowView={this._renderRowView}
+//           onFetch={this._onFetch}
+//           onPress={this._onPress}
+//           firstLoader={true} // display a loader for the first fetching
+//           pagination={true} // enable infinite scrolling using touch to load more
+//           refreshable={true} // enable pull-to-refresh for iOS and touch-to-refresh for Android
+//           withSections={false} // enable sections
+//           customStyles={{
+//             paginationView: {
+//               backgroundColor: '#eee',
+//             },
+//           }}
+
+//           refreshableTintColor="blue"
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// var styles = {
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#FFF',
+//   },
+//   navBar: {
+//     height: 64,
+//     backgroundColor: '#CCC'
+//   },
+//   row: {
+//     padding: 10,
+//     height: 44,
+//   },
+// };
 
 AppRegistry.registerComponent('weather', () => weather);
