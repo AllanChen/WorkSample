3efe073d3db35051f17084451f8a7c7b26650193
Allan <cyjian0@gmail.com>
2016-09-27 14:50:34 +0800

    ScrollView Done

--- ReactNative/weathers/start.js
+++ ReactNative/weathers/start.js
@@ -9,62 +9,46 @@ import {
 } from 'react-native';
 import Dimensions from 'Dimensions';
 import PageOne from './Section/pageOne.js'
-import PageTwo from './Section/pageTwo.js'
-import AddPaging from 'react-native-paged-scroll-view'
-// var PagedScrollView = AddPaging(ScrollView)
 const swidth = Dimensions.get('window').width;
 const sheight = Dimensions.get('window').height;
 
 class start extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+
+        };
     }
 
-// handlePageChange (state) {
-//     console.log('current horizontal page:', state.currentHorizontalPage)
-//     console.log('current vertical page:  ', state.currentVerticalPage)
-//     console.log('total horizontal pages: ', state.totalHorizontalPages)
-//     console.log('total vertical pages:   ', state.totalVerticalPages)
-//   }
     render() {
         return (
-            <View style={styles.container}>
-                <Image
-                    source = {require('./images/bg.jpg') }
-                    >
-                    <View style={styles.headView}>
+            <Image
+                style={styles.bgImage}
+                source = {require('./images/bg.jpg') }
+                >
+                <View style={styles.headView}>
                         <Text style={{ textAlign: 'center', color: '#ffffff', fontSize: 23 }}> 广州 </Text>
                     </View>
-                    /*
-                    <ScrollView style={styles.scrollViewStyle}
-                                automaticallyAdjustContentInsets={false}
-                                horizontal = {false}
-                                vertical = {true}
-                                pagingEnabled = {true}
-                                showsVerticalScrollIndicator={true}
-                                showsHorizontalScrollIndicator={false}                        
-                                contentContainerStyle={{height: sheight-20, alignItems: 'flex-start'}}
-                                ref="scrollView"
-                    >
+                <View style={styles.scrollViewContainer}>
+                    <ScrollView 
+                        showsVerticalScrollIndicator={false}
+                        showsVerticalScrollIndicator={false}
+                        contentContainerStyle={styles.contentContainer}>
                         <PageOne />
                         <PageOne />
                     </ScrollView>
-                    */
-                    // <PagedScrollView onPageChange={this.handlePageChange.bind(this)}>
-                    //     <PageOne />
-                    //     <PageOne />
-                    // </PagedScrollView>
-                </Image>
-            </View>
+                </View>
+            </Image>
         );
     }
+
 
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
+    scrollViewContainer: {
+        height: sheight,
+        backgroundColor: 'rgba(0,0,0,0)',
     },
 
     bgImage: {
@@ -80,11 +64,8 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0,0,0,0)',
     },
 
-    scrollViewStyle: {
-        flex:1,
-        flexDirection: 'column',
-        height:sheight-20,
-    },
-
+    contentContainer: {
+        backgroundColor: "rgba(0,0,0,0)",
+    }
 });
 module.exports = start;
