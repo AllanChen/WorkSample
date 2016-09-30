f5f764a2e85336ba575c8e038cb7d3a02217a58f
Allan <cyjian0@gmail.com>
2016-09-30 16:02:17 +0800

    左侧导航栏已经完成，只差网络请求

---
+++ ReactNative/weathers/Section/citylist.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import g from '../Config/config.js'
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+    ListView,
+    Image
+} from 'react-native';
+let addressData = require('../Storage/storage.json');
+let rowHeight;
+export default class citylist extends Component {
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            dataSource: ds.cloneWithRows(addressData),
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{ width: 100, height: 18, backgroundColor: '#131313' }}></View>
+                <ListView
+                    renderRow = {this.renderRow}
+                    automaticallyAdjustContentInsets = {false}
+                    enableEmptySections = {true}
+                    showsVerticalScrollIndicator={false}
+                    dataSource = {this.state.dataSource}
+                    />
+            </View>
+        );
+    }
+
+    renderRow(rowData: string, sectionID: number, rowID: number, highlightRow: (sectionID: number, rowID: number) => void) {
+        let imageHeight = 50;
+        if (rowID == 0) imageHeight = 70;
+        return (
+            <TouchableHighlight
+                onPress ={() => {
+                } }
+                underlayColor = '#003a7a'
+                >
+                <View style={styles.row}>
+                    <Text style={{ color: 'white', fontSize: 16 }}>{addressData[rowID].name}</Text>
+                </View>
+
+            </TouchableHighlight>
+        );
+    }
+
+
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: 100,
+        height: g.SHEIGHT,
+    },
+
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent:'center',
+        backgroundColor: '#222122',
+        height:50,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(255,255,255,.3)',
+    },
+
+    contentBox: {
+        height: 40,
+    },
+
+    firstRowText: {
+        paddingTop: 25,
+    },
+});
--- ReactNative/weathers/start.js
+++ ReactNative/weathers/start.js
@@ -6,14 +6,30 @@ import {
     View,
     Image,
     ScrollView,
+    TouchableHighlight,
 } from 'react-native';
 import Dimensions from 'Dimensions';
 import PageOne from './Section/pageOne.js'
 import PageTwo from './Section/pageTwo.js'
 import g from './Config/config.js'
+import Citylist from './Section/citylist.js'
+import Button from './Common/Button.js'
+import Drawer from 'react-native-drawer'
+
 class start extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            drawerOpen: false,
+            drawerDisabled: false,
+        };
+
+        closeDrawer = () => {
+            this._drawer.close()
+        };
+        openDrawer = () => {
+            this._drawer.open()
+        };
     }
 
     render() {
@@ -22,37 +38,56 @@ class start extends Component {
                 style={styles.bgImage}
                 source = {require('./images/bg.jpg') }
                 >
-                <View style={styles.headView}>
-                    <Text style={{ textAlign: 'center', color: '#ffffff', fontSize: 23 }}> 广州 </Text>
-                </View>
-                <View style={styles.scrollViewContainer}>
-                <ScrollView
-                        automaticallyAdjustContentInsets={false}
-                        showsVerticalScrollIndicator={false}
-                        showsHorizontalScrollIndicator={false}
-                        horizontal={true}
-                        pagingEnabled = {true}
-                        >
+                <Drawer
+                    ref={(ref) => this._drawer = ref}
+                    type="displace"
+                    content={ <Citylist /> }
+                    tapToClose={true}
+                    openDrawerOffset={0.7} // 20% gap on the right side of drawer
+                    panCloseMask={0.7}
+                    >
 
-                    <ScrollView
-                        automaticallyAdjustContentInsets={false}
-                        showsVerticalScrollIndicator={false}
-                        showsHorizontalScrollIndicator={false}
-                        contentContainerStyle={styles.contentContainer}>
-                        <PageOne />  
-                        <PageTwo />         
-                    </ScrollView>
+                    <View style={styles.headView}>
+                        <TouchableHighlight
+                            onPress = {() => {
+                                this._drawer.open();
+                            } }
+                            underlayColor = 'rgba(0,0,0,0)'
+                            >
+                            <Image style={{ width: 20, height: 20, marginLeft: 10 }} source = {require('./images/add.png') }></Image>
+                        </TouchableHighlight>
+                        <Text style={{ textAlign: 'center', color: '#ffffff', fontSize: 18 }}> 广州 </Text>
+                        <View style={{ width: 20, height: 20 }}></View>
+                    </View>
+                    <View style={styles.scrollViewContainer}>
+                        <ScrollView
+                            automaticallyAdjustContentInsets={false}
+                            showsVerticalScrollIndicator={false}
+                            showsHorizontalScrollIndicator={false}
+                            horizontal={true}
+                            pagingEnabled = {true}
+                            >
 
-                    <ScrollView
-                        automaticallyAdjustContentInsets={false}
-                        showsVerticalScrollIndicator={false}
-                        showsHorizontalScrollIndicator={false}
-                        contentContainerStyle={styles.contentContainer}>
-                        <PageOne />  
-                        <PageTwo />         
-                    </ScrollView>
-                </ScrollView>
-                </View>
+                            <ScrollView
+                                automaticallyAdjustContentInsets={false}
+                                showsVerticalScrollIndicator={false}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerStyle={styles.contentContainer}>
+                                <PageOne />
+                                <PageTwo />
+                            </ScrollView>
+
+                            <ScrollView
+                                automaticallyAdjustContentInsets={false}
+                                showsVerticalScrollIndicator={false}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerStyle={styles.contentContainer}>
+                                <PageOne />
+                                <PageTwo />
+                            </ScrollView>
+                        </ScrollView>
+                    </View>
+                </Drawer>
             </Image>
         );
     }
@@ -60,9 +95,8 @@ class start extends Component {
 
 const styles = StyleSheet.create({
     scrollViewContainer: {
-        height: g.SHEIGHT-50,
+        height: g.SHEIGHT - 50,
         backgroundColor: 'rgba(0,0,0,0)',
-        marginTop:15,
     },
 
     bgImage: {
@@ -71,15 +105,19 @@ const styles = StyleSheet.create({
     },
 
     headView: {
-        paddingTop: 30,
+        backgroundColor: "rgba(0,0,0,0)",
+        flexDirection: 'row',
         width: g.SWIDTH,
-        height: 20,
-        justifyContent: 'center',
-        backgroundColor: 'rgba(0,0,0,0)',
+        marginTop: 25,
+        height: 25,
+        justifyContent: 'space-between'
     },
 
     contentContainer: {
         backgroundColor: "rgba(0,0,0,0)",
-    }
+    },
+
+    drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3,  },
+    main: { paddingLeft: 3 },
 });
 module.exports = start;
